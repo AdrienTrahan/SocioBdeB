633c88640c6becef52eaae1b72ca51bfad7f2a56
Adrien Trahan <adrientrahan265@gmail.com>
2023-03-28 23:03:59 -0400

    another release

--- build/_app/immutable/chunks/1.25b5a239.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.2323b5bf.js";export{t as component};
---
+++ build/_app/immutable/chunks/1.827b882e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.92cdb793.js";export{t as component};
